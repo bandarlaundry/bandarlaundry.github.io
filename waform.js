d2d55b31a57a338622599abf14b69a4a91c08925
Bandar Laundry <bandarlaundry@gmail.com>
2025-03-11 22:17:07 +0700

    Delete waform.js

--- waform.js
+++
@@ -1,7 +0,0 @@
- document.getElementById('whatsappForm').addEventListener('submit', function(e) {
-            e.preventDefault();
-            const formData = new FormData(this);
-            const message = `Name: ${formData.get('name')}\nEmail: ${formData.get('email')}\nPhone: ${formData.get('phone')}\nAddress: ${formData.get('address')}\nMessage: ${formData.get('message')}`;
-            const whatsappUrl = `https://wa.me/6285773009666?text=${encodeURIComponent(message)}`;
-            window.open(whatsappUrl, '_blank');
-        });
