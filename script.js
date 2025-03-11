349333d4ba18ed27b8bba1a393723cc27ef98304
Bandar Laundry <bandarlaundry@gmail.com>
2025-03-11 13:51:59 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-
-document.querySelectorAll('.link').forEach(link => {
-    link.addEventListener('click', () => {
-        console.log(`Clicked on ${link.href}`);
-        // You can add more functionality here, like tracking clicks or opening links in new tabs.
-    });
-});
